8753edf0c758e9b6e5f01483ca6c5dfa8bf24915
jgravalo <jesusgrawork361@gmail.com>
2023-11-29 16:56:18 +0100

    mejora de texturas

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: jgravalo <jgravalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 18:39:16 by jgravalo          #+#    #+#             */
-/*   Updated: 2023/11/29 15:15:41 by jgravalo         ###   ########.fr       */
+/*   Updated: 2023/11/29 16:55:12 by jgravalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	set_texture(char *addr, int line, int w, t_frame *data)
 	int	color;
 
 	//printf("addr: <%s>, line: %d, j: %d\n", addr, line, j);
-	addr += line * data->j;
+	addr += line * data->j; // ajustamos addr para la coordenada vertical
 	data->sp = (float)data->r / (float)(line / 4);
 	if ((float)data->r < (float)(line / 4))
 		data->w = w * 4;
@@ -30,7 +30,7 @@ int	set_texture(char *addr, int line, int w, t_frame *data)
 		data->w = w / 4;
 	else
 		data->w = w;
-	addr += data->w * 4;
+	addr += data->w * 4; // ajustamos addr para la coordenada horizontal
 	//printf("r = %f, line = %d, sp = %f ", data->r, line, data->sp);
 	color = char_to_int(addr[3], addr[2], addr[1], addr[0]);
 	return (color);
@@ -44,21 +44,24 @@ static void	draw_line(t_cub *cub, int w, t_img *frame, t_img *dir)
 	float	y;
 	float	j2;
 
+	//if (cub->draw_start == 0) j = {} ; //para ajustar la vista cerca del muro
 	j = 0;
-	data.r = cub->draw_end - cub->draw_start;
+	data.r = cub->draw_end - cub->draw_start; // calculamos tama
 	while (j < dir->line / 4)
 	{
 		data.j = j;
 		color = set_texture(dir->addr, dir->line, w, &data);
-		/**/for (j2 = 0; j2 < data.sp; j2++)/**/
+		j2 = 0;
+		while (j2 < data.sp)/**/
 		{
 			//printf("j = %f\n", j);
 			y = cub->draw_start + j * data.sp;
 			//printf("y = %f\n", y);
 			if (y < H)
 				my_mlx_pixel_put(frame, w, y, color);
+			j2++;
 		}
-		j++;
+		j += 0.1;
 	}
 }
 
