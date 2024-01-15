7673b1d422808f674f961872056186d624ff17f0
Diego Tome Perez <dtome-pe@cbr1s2.42barcelona.com>
2024-01-15 13:43:26 +0100

    anadimos texturas y mapas para debugear

--- src/parse.c
+++ src/parse.c
@@ -6,21 +6,32 @@
 /*   By: dtome-pe <dtome-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 18:56:11 by dtome-pe          #+#    #+#             */
-/*   Updated: 2024/01/11 16:06:34 by dtome-pe         ###   ########.fr       */
+/*   Updated: 2024/01/15 13:41:20 by dtome-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-static void	element_aux(char **path, char **split, int *flag)//, int type)
-{
-	if (!(*path))
+static void	element_aux(char **path, char **split, int *flag, int color)//, int type)
+{	
+	int	i;
+
+	i = 0;
+	if (!color)
 	{
-		(*flag)--;
-		*path = ft_substr(split[1], 0, ft_strlen(split[1]) - 1);
-		return ;
+		if (!(*path))
+		{
+			(*flag)--;
+			*path = ft_substr(split[1], 0, ft_strlen(split[1]) - 1);
+			return ;
+		}
+		*flag = 2;
 	}
-	*flag = 2;
+	else
+	{
+		
+	}
+	
 }
 
 static int	get_element(t_cub *cub, char *line)
@@ -31,17 +42,17 @@ static int	get_element(t_cub *cub, char *line)
 	flag = 1;
 	split = ft_splitchr(line, " \t");
 	if (ft_strcmp(split[0], "NO") == 0)
-		element_aux(&cub->n_p, split, &flag);
+		element_aux(&cub->n_p, split, &flag, 0);
 	else if (ft_strcmp(split[0], "SO") == 0)
-		element_aux(&cub->s_p, split, &flag);
+		element_aux(&cub->s_p, split, &flag, 0);
 	else if (ft_strcmp(split[0], "WE") == 0)
-		element_aux(&cub->w_p, split, &flag);
+		element_aux(&cub->w_p, split, &flag, 0);
 	else if (ft_strcmp(split[0], "EA") == 0)
-		element_aux(&cub->e_p, split, &flag);
+		element_aux(&cub->e_p, split, &flag, 0);
 	else if (ft_strcmp(split[0], "C") == 0)
-		element_aux(&cub->ceiling_color, split, &flag);
+		element_aux(&cub->ceiling_color, split, &flag, 1);
 	else if (ft_strcmp(split[0], "F") == 0)
-		element_aux(&cub->floor_color, split, &flag);
+		element_aux(&cub->floor_color, split, &flag, 1);
 	ft_free_m(split);
 	return (flag);
 }
